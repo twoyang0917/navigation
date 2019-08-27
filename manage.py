3f039135749f2f8da2d2ae00575600ed851cd1ff
yangyang <yangyangjishu@blued.com>
2019-08-27 18:55:53 +0800

    init change

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "daohang.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "navigation.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
--- navigation/wsgi.py
+++ navigation/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for daohang project.
+WSGI config for navigation project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "daohang.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "navigation.settings")
 
 application = get_wsgi_application()
